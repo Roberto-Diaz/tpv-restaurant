41215d12da48de8838146c8dbccb61c2470477e6
Roberto-Diaz <robertodd07@gmail.com>
2019-07-14 15:03:12 -0500

    Utilidades para mvvm

---
+++ Restaurant/Utility/ObservableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Utility
+{
+    public class ObservableObject: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(params string[] name)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var n in name)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(n));
+                }
+            }
+        }   
+    }
+}
---
+++ Restaurant/Utility/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restaurant.Utility
+{
+    public class RelayCommand: ICommand
+    {   
+        private Action methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+        readonly Predicate<Object> _canExecute;
+
+        public RelayCommand(Action methodToExecute, Predicate<Object> canExecute)
+        {
+            if (methodToExecute == null)
+                throw new NullReferenceException("execute");
+
+            this.methodToExecute = methodToExecute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action methodToExecute) : this(methodToExecute, null)
+        {
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            methodToExecute.Invoke();
+        }
+    }
+}
